b6fc6403293aaeb5a2585a58364daa12030feeee
Koloina Rajaonarivony <koloina.arilala@gmail.com>
2025-07-07 11:58:37 +0300

    creation modele

--- prediction_prix.py
+++ prediction_prix.py
@@ -1,3 +1,20 @@
 import pandas as pd;
+from sklearn.linear_model import LinearRegression
 train_data= pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
+
+#Supposons ceci comme colonnes pertinentes
+features = ["Street","Neighborhood","Electrical","TotRmsAbvGrd"]
+target = "SalePrice"
+
+x_train = train_data[features]
+y_train = train_data[target]
+
+x_test = test_data[features]
+
+x_train = x_train.fillna(0)
+x_test = x_test.fillna(0)
+
+model = LinearRegression()
+model.fit(x_train, y_train)
+
