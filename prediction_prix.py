94eb9d12dd8510d35643a6830ba3f6b88e9a7aa8
Koloina Rajaonarivony <koloina.arilala@gmail.com>
2025-07-10 16:21:18 +0300

    modele avec choix des colonnes automatique

--- prediction_prix.py
+++ prediction_prix.py
@@ -1,33 +1,67 @@
 import pandas as pd;
 from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
 train_data= pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 
 #Supposons ceci comme colonnes pertinentes
-features = ["Street","Neighborhood","Electrical","TotRmsAbvGrd"]
+# features = ["Street","Neighborhood","Electrical","TotRmsAbvGrd"]
+# target = "SalePrice"
+
+# x_train = train_data[features]
+# y_train = train_data[target]
+
+# x_test = test_data[features]
+
+# x_train = x_train.fillna(0)
+# x_test = x_test.fillna(0)
+
+# # print(x_train.dtypes)
+# #mampitovy ny variable ho type entier daholo
+# x_train = pd.get_dummies(x_train)
+# x_test = pd.get_dummies(x_test)
+
+# # Aligner les colonnes entre train et test
+# x_train, x_test = x_train.align(x_test, join='left', axis=1, fill_value=0)
 target = "SalePrice"
 
-x_train = train_data[features]
-y_train = train_data[target]
+# 2. On sépare les features et la target
+X = train_data.drop(columns=[target])
+y = train_data[target]
 
-x_test = test_data[features]
+# 3. On identifie les colonnes numériques et catégorielles
+num_cols = X.select_dtypes(include=['int64', 'float64']).columns
+cat_cols = X.select_dtypes(include=['object']).columns
 
-x_train = x_train.fillna(0)
-x_test = x_test.fillna(0)
+# 4. Prétraitement : Imputer et encoder
+preprocessor = ColumnTransformer([
+    ('num', SimpleImputer(strategy='median'), num_cols),
+    ('cat', Pipeline([
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ]), cat_cols)
+])
 
-# print(x_train.dtypes)
-#mampitovy ny variable ho type entier daholo
-x_train = pd.get_dummies(x_train)
-x_test = pd.get_dummies(x_test)
+# 5. Pipeline de transformation uniquement pour les données
+X_processed = preprocessor.fit_transform(X)
+X_test_processed = preprocessor.transform(test_data)
 
-# Aligner les colonnes entre train et test
-x_train, x_test = x_train.align(x_test, join='left', axis=1, fill_value=0)
+# 6. Sélection automatique des meilleures features (ex : les 50 meilleures)
+selector = SelectKBest(score_func=f_regression, k=50)
+X_selected = selector.fit_transform(X_processed, y)
+X_test_selected = selector.transform(X_test_processed)
 
 model = LinearRegression()
-model.fit(x_train, y_train)
+# model.fit(x_train, y_train)
+model.fit(X_selected, y)
 
 # # Prédiction
-y_pred = model.predict(x_test)
+y_pred = model.predict(X_test_selected)
 
 submission = pd.DataFrame({
     "Id": test_data["Id"],
