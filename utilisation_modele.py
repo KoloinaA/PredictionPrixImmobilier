198d00501449b13d74687bdd32bf97d081a45cfd
Koloina Rajaonarivony <koloina.arilala@gmail.com>
2025-07-13 18:16:22 +0300

    utilisation model model_prix_immobilier

--- utilisation_modele.py
+++ utilisation_modele.py
@@ -1,4 +1,19 @@
 import pandas as pd
 import joblib
 
-model = joblib.load("model_prix_immobilier.pkl")
+model = joblib.load("model_prix_immobilier")
+preprocessor = joblib.load("preprocessor.pkl")
+selector = joblib.load("selector.pkl")
+
+new_data = pd.read_csv("test.csv")
+X_processed = preprocessor.transform(new_data)
+X_selected = selector.transform(X_processed)
+
+y_pred = model.predict(X_selected)
+
+submission = pd.DataFrame({
+    "Id" : new_data["Id"],
+    "SalePrice": y_pred
+})
+
+submission.to_csv("submission_new.csv", index = False)
